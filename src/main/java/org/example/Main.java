a619710cb60bd2caa347b7d6b1b8e5bf5dfcc72d
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-10-03 16:24:03 +0530

    Update Main.java
    fn

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,7 +30,9 @@ public String display()
 public String owner()
 {
 
-    return "@Satyam Patel  deployed java application Azure Successfully ";
+   // return "@Satyam Patel  deployed java application Azure Successfully ";
+   return "Hiii Ishika , Thoda aaram kr le thak gyi hogi";
+    
 
 }
 
